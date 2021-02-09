2a9740c87dff06b0a1449cfdef007266e1f3ba33
Bernardo Alves Roballo <bernardo_roballo@hotmail.com>
2021-02-09 04:29:21 -0300

    Atualização da atualização do endereço.
    Signed-off-by: Bernardo Alves Roballo <bernardo_roballo@hotmail.com>

--- app/Controllers/Endereco.php
+++ app/Controllers/Endereco.php
@@ -8,6 +8,30 @@ class Endereco extends GenericController
 	{
 	}
 
-	//--------------------------------------------------------------------
-
+	public function pegaEnderecoUsuario()
+	{
+		// Pega os dados;
+		$request = $this->request->getJSON(true);
+		$enderecoModel = new \App\Models\EnderecoModel();
+		$dados = $enderecoModel->pegaEnderecoUsuario($request['uid']);
+		$this->decodeJson(array('usuario'), $dados);
+		echo json_encode($dados);
+	}
+	/**
+	 * Insere ou atualiza um produto de um determinado usuário
+	 *
+	 * @return void
+	 */
+	public function insereAtualizaEndereco()
+	{
+		// Pega os dados;
+		$request = $this->request->getJSON(true);
+		$enderecoModel = new \App\Models\EnderecoModel();
+		// Verifica se já existe um usuário com endereço se tiver, apenas atualiza os dados
+		if (empty($request['endereco']['usuario']) || empty($enderecoModel->lerEspecifico(array('usuario' => $request['endereco']['usuario'])))) {
+			$enderecoModel->inserir($request['endereco']);
+		} else {
+			$enderecoModel->atualizar($request['endereco'], array('usuario' => $request['endereco']['usuario']));
+		}
+	}
 }
--- app/Models/EnderecoModel.php
+++ app/Models/EnderecoModel.php
@@ -13,4 +13,11 @@ class EnderecoModel extends GenericModel
   {
     parent::__construct('tbEndereco');
   }
+  public function pegaEnderecoUsuario($uid)
+  {
+    $select = "{$this->table}.*, JSON_OBJECT('uid', tbUsuario.uid, 'nome', tbUsuario.nome, 'tipoUsuario', JSON_OBJECT('id', tbTipoUsuario.id, 'tipo', tbTipoUsuario.tipo)) AS usuario";
+    $query = $this->db->table($this->table)->select($select)->join('tbUsuario', 'tbUsuario.uid = tbEndereco.usuario')
+      ->join('tbTipoUsuario', 'tbTipoUsuario.id = tbUsuario.tipoUsuario')->where('tbUsuario.uid', $uid)->get();
+    return $query->getResult();
+  }
 }
